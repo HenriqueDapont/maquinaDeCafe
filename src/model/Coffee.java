b96b775aa2f3ab3e40d0a8565775d5037e2bc57d
Henrique <henrique5044@gmail.com>
2022-09-05 14:57:34 -0300

    condicao do for alterada

--- src/model/Coffee.java
+++ src/model/Coffee.java
@@ -48,7 +48,7 @@ public class Coffee {
             result = "Troco = R$" + String.format("%.2f",change) + "\n";
             value = (int) change;
 
-            for (int i = 0; value != 0 && value != 1; i++) {
+            for (int i = 0; value >= 2; i++) {
                 qtNotes = value / notes.get(i);
                 if (qtNotes != 0) {
                     result = result + (qtNotes + " nota(s) de R$" + notes.get(i) + ",00.\n");
@@ -62,7 +62,7 @@ public class Coffee {
 
             changeRemain = (int) Math.round((change - (int) change) * 100);
 
-            for (int i = 1; changeRemain != 0 && changeRemain >= 5; i++) {
+            for (int i = 1; changeRemain >= 5; i++) {
                 qtCoins = changeRemain / coins.get(i);
                 if (qtCoins != 0) {
                     result = result + (qtCoins + " moeda(s) de " + coins.get(i) + " centavos.\n");
